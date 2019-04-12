97072603e02d165facf5e485359c550347e5e14b
qiao <1170793857@qq.com>
2019-04-12 11:39:22 +0800

    ssm框架项目

---
+++ SSM/src/com/mapper/UserMapper.java
@@ -0,0 +1,38 @@
+package com.mapper;
+
+import com.pojo.User;
+import com.pojo.UserExample;
+import com.pojo.UserKey;
+import com.pojo.UserWithBLOBs;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    int countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(UserWithBLOBs record);
+
+    int insertSelective(UserWithBLOBs record);
+
+    List<UserWithBLOBs> selectByExampleWithBLOBs(UserExample example);
+
+    List<User> selectByExample(UserExample example);
+
+    UserWithBLOBs selectByPrimaryKey(UserKey key);
+
+    int updateByExampleSelective(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") UserWithBLOBs record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(UserWithBLOBs record);
+
+    int updateByPrimaryKeyWithBLOBs(UserWithBLOBs record);
+
+    int updateByPrimaryKey(User record);
+}
